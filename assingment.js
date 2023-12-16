6ba009a6865919ee5ae030e67cb474d57da1e82e
cjnj <151698129+cjnj@users.noreply.github.com>
2023-12-16 09:20:05 -0500

    Add files via upload

---
+++ assingment.js
@@ -0,0 +1,75 @@
+//netflix variables
+let title="birdbox"
+let genre="thriller"
+let rating=4.5
+let runtime=120
+
+//realtor.com variables
+let buy="buy a home";
+let sell="sell a home";
+let rent="rent a home";
+let mortgage="mortgage rates";
+let advertise="avertise your property";
+
+//Etsy variables
+let categories="types of products";
+let visa="payment option"
+let store="luna art print"
+let description="Vintage Art Sales"
+let Sales=196
+
+//Amazon variables
+let buyAgain="re-purchase"
+let coupons="discounts"
+
+//wikipedia variables
+let english=6755000
+let espanol=1913000
+let wiktionary="Free Dictionary";
+let wikidata="Freed knowledge base";
+
+console.log(title);
+console.log(genre);
+console.log(rating);
+console.log(runtime);
+console.log(buy);
+console.log(sell);
+console.log(rent);
+console.log(mortgage);
+console.log(advertise);
+console.log(categories);
+console.log(visa);
+console.log(store);
+console.log(description);
+console.log(Sales);
+console.log(buyAgain);
+console.log(coupons);
+console.log(english);
+console.log(espanol);
+console.log(wiktionary);
+console.log(wikidata);
+
+
+
+document.write(`<p> ${description} </p>`);
+document.write(`<p> ${title} </p>`);
+document.write(`<p> ${genre} </p>`);
+document.write(`<p> ${rating} </p>`);
+document.write(`<p> ${runtime} </p>`);
+document.write(`<p> ${buy} </p>`);
+document.write(`<p> ${sell} </p>`);
+document.write(`<p> ${rent} </p>`);
+document.write(`<p> ${mortgage} </p>`);
+document.write(`<p> ${advertise} </p>`);
+document.write(`<p> ${categories} </p>`);
+document.write(`<p> ${visa} </p>`);
+document.write(`<p> ${store} </p>`);
+document.write(`<p> ${description} </p>`);
+document.write(`<p> ${Sales} </p>`);
+document.write(`<p> ${buyAgain} </p>`);
+document.write(`<p> ${coupons} </p>`);
+document.write(`<p> ${english} </p>`);
+document.write(`<p> ${espanol} </p>`);
+document.write(`<p> ${wiktionary} </p>`);
+document.write(`<p> ${wikidata} </p>`);
+
